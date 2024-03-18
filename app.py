c8e9fcac05b5505234b8303e6f2f7bcf6281a509
Addicted <addictedchrysler@gmail.com>
2024-03-18 20:59:42 +0000

    Add: routes to html pages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template("login.html")
@@ -12,6 +13,17 @@ def hello_world():
 def register():
     return render_template("register.html")
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("home.html")
+
+
+@app.route("/expense")
+def addexpense():
+    return render_template("addexpense.html")
+
+
+@app.route("/manageexpense")
+def manageexpense():
+    return render_template("manageexpense.html")
